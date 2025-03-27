dce7d3a98c484776143f59bf72328ad8594d0fd7
Huy Danh <huynd.gds@gmail.com>
2025-03-27 17:50:52 +0000

    Deploying build to gh-pages

---
+++ precache-manifest.38ad96e2780e5a5cd04ff8aeb985560f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40a5805273be53f5f7373ff150d5501",
+    "url": "/showTiviv1/index.html"
+  },
+  {
+    "revision": "c408d991f910f72393dd",
+    "url": "/showTiviv1/static/css/main.c4724098.chunk.css"
+  },
+  {
+    "revision": "305b979d1f3a75e507d4",
+    "url": "/showTiviv1/static/js/2.f40764b9.chunk.js"
+  },
+  {
+    "revision": "c408d991f910f72393dd",
+    "url": "/showTiviv1/static/js/main.2458212e.chunk.js"
+  },
+  {
+    "revision": "610404b61dadb7f8fbf2",
+    "url": "/showTiviv1/static/js/runtime~main.a2881c66.js"
+  }
+]);
